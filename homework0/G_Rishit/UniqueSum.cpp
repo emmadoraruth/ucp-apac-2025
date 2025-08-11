e9eeeda96455342210bcb7fbff7d54189b174ae1
RishitGG <rishitgutta@gmail.com>
2025-08-11 07:06:22 +0530

    homework0 submission by G Rishit

---
+++ homework0/G_Rishit/UniqueSum.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int UniqueSum(const vector<int>& nums,int n){
+    unordered_map<int,bool> umap;
+    int sum=0;
+    for (int i=0; i<n; i++) {
+        int j= nums[i];
+        if (!umap[j]){      //if umap[j]==0 the number at index j has not been encountered before in the array and hence is unique
+            sum+=j; 
+            umap[j]=true;
+        }
+    }
+    return sum;
+}
+
+int main(){
+    vector<int> nums;
+    int n;
+    cin>>n;
+    for (int i=0; i<n; i++){
+        int num;
+        cin>>num;
+        nums.push_back(num);
+    }
+
+    cout<< UniqueSum(nums, n)<< endl;
+    return 0;
+}
+//took me about 10 minutes to solve this question and write the code for it
+// T.C O(n)   S.C O(n)
---
+++ homework0/G_Rishit/ZeroSum.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int ZeroSum(vector<int>& nums, int n){
+    unordered_map<int, int> freq;
+    for (int i=0; i<n; i++){
+        freq[nums[i]]++;
+    }
+
+    int ans=0;
+    for (auto& x:freq) {
+        int num=x.first;
+        if (num>=0) {
+            if (num==0){
+                ans+=freq[0]/2;
+            } 
+            else{
+                auto it=freq.find(-num);
+                if (it!=freq.end()){
+                    ans+=min(x.second, it->second);
+                }
+            }
+        }
+    }
+    return ans;
+}
+
+int ZeroSumFollowUp(const vector<int>& nums, int n){
+    unordered_map<int, int> seen;
+    int ans=0;
+    for (int i=0; i<n; i++) {
+        ans+=seen[-nums[i]];
+        seen[nums[i]]++;
+    }
+    return ans;
+}
+
+int main(){
+    vector<int> nums;
+    int n;
+    cin>>n;
+    for (int i=0; i<n; i++){
+        int num;
+        cin>>num;
+        nums.push_back(num);
+    }
+
+    cout<< ZeroSum(nums, n)<< endl;
+    cout<< ZeroSumFollowUp(nums, n)<< endl;
+    return 0;
+}
