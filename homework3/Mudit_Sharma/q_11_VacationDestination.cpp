3ffa77f80e0122ec2c8adfb9f0436754590cd5df
Mudit Sharma <muditsharma332@gmail.com>
2026-01-24 14:29:09 +0530

    Upadte q11 by Mudit Sharma

--- homework3/Mudit_Sharma/q_11_VacationDestination.cpp
+++ homework3/Mudit_Sharma/q_11_VacationDestination.cpp
@@ -17,52 +17,51 @@
 // Output: 2 (["Boston", "Philadelphia", "Washington, D.C", "Newport", "Harper's Ferry", "Portland"]) 
 
 #include <bits/stdc++.h>
-using namespace std;
 
 int findDestinations(
-    vector<pair<string, pair<string,double>>>& mapping,
-    string origin,
+    std::vector<std::pair<std::string, std::pair<std::string,double>>>& mapping,
+    std::string origin,
     double k
 ) {
     // Build undirected graph
-    unordered_map<string, vector<pair<string, double>>> adj;
+    std::unordered_map<std::string, std::vector<std::pair<std::string, double>>> adj;
 
     for (size_t i = 0; i < mapping.size(); i++) {
-        string u = mapping[i].first;
-        string v = mapping[i].second.first;
+        std::string u = mapping[i].first;
+        std::string v = mapping[i].second.first;
         double wt = mapping[i].second.second;
 
-        adj[u].push_back(make_pair(v, wt));
-        adj[v].push_back(make_pair(u, wt));
+        adj[u].push_back(std::make_pair(v, wt));
+        adj[v].push_back(std::make_pair(u, wt));
     }
 
     // Distance map
-    unordered_map<string, double> dist;
+    std::unordered_map<std::string, double> dist;
     for (auto it = adj.begin(); it != adj.end(); ++it) {
         dist[it->first] = DBL_MAX;
     }
 
     // Min-heap (time, city)
-    priority_queue<
-        pair<double, string>,
-        vector<pair<double, string>>,
-        greater<pair<double, string>>
+    std::priority_queue<
+        std::pair<double, std::string>,
+        std::vector<std::pair<double, std::string>>,
+        std::greater<std::pair<double, std::string>>
     > pq;
 
     dist[origin] = 0.0;
-    pq.push(make_pair(0.0, origin));
+    pq.push(std::make_pair(0.0, origin));
 
     while (!pq.empty()) {
-        pair<double, string> top = pq.top();
+        std::pair<double, std::string> top = pq.top();
         pq.pop();
 
         double currTime = top.first;
-        string u = top.second;
+        std::string u = top.second;
 
         if (currTime > dist[u]) continue;
 
         for (size_t i = 0; i < adj[u].size(); i++) {
-            string v = adj[u][i].first;
+            std::string v = adj[u][i].first;
             double wt = adj[u][i].second;
 
             double newTime = currTime + wt;
@@ -72,7 +71,7 @@ int findDestinations(
 
             if (newTime < dist[v]) {
                 dist[v] = newTime;
-                pq.push(make_pair(newTime, v));
+                pq.push(std::make_pair(newTime, v));
             }
         }
     }
@@ -89,7 +88,7 @@ int findDestinations(
 }
 
 int main() {
-    vector<pair<string, pair<string, double>>> mapping = {
+    std::vector<std::pair<std::string, std::pair<std::string, double>>> mapping = {
         {"Boston", {"New York", 4}},
         {"New York", {"Philadelphia", 2}},
         {"Boston", {"Newport", 1.5}},
@@ -98,11 +97,11 @@ int main() {
         {"Philadelphia", {"Washington, D.C.", 2.5}}
     };
 
-    string origin = "New York";
+    std::string origin = "New York";
 
-    cout << findDestinations(mapping, origin, 5) << endl; // 2
-    cout << findDestinations(mapping, origin, 7) << endl; // 4
-    cout << findDestinations(mapping, origin, 8) << endl; // 6
+    std::cout << findDestinations(mapping, origin, 5) << std::endl; // 2
+    std::cout << findDestinations(mapping, origin, 7) << std::endl; // 4
+    std::cout << findDestinations(mapping, origin, 8) << std::endl; // 6
 
     return 0;
 }
